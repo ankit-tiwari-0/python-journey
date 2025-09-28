7711d878dc494fc7ae24bad54d435603c120e04c
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2025-09-28 20:44:10 +0530

    datatype

---
+++ 02_datatypes/chapter5.py
@@ -0,0 +1,31 @@
+#list [] ---> array ,this  is mutable
+ingre = ["water", "milk","black tea"]
+ingre.append("sugar")
+print(f"Ingrediants are : {ingre}")
+ingre.remove("water")
+print(f" New ingrediants are : {ingre}")
+
+spice_option = ["ginger", "cardamom"]
+chai_ing = ["water","milk"]
+#we use exted to add both
+
+chai_ing.extend(spice_option)
+print(f"chai: {chai_ing}")
+
+chai_ing.insert(2, "elachi")
+print(f"chai: {chai_ing}")
+
+last_added = chai_ing.pop()
+print(f"chai4: {last_added}")
+print(f"chai: {chai_ing}")
+chai_ing.reverse()
+print(f"chai: {chai_ing}")
+chai_ing.sort()
+print(f"chai: {chai_ing}")
+
+sugar_level = [1,2,3,4,5,6]
+print(f"maximum sugar level: {max(sugar_level)}")
+print(f"maximum sugar level: {min(sugar_level)}")
+
+
+
