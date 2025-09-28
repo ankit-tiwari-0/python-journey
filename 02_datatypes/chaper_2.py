578eb81d9f5e5370fc94da1fc2ae94f379887255
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2025-09-28 16:59:50 +0530

    python basic

---
+++ 02_datatypes/chaper_2.py
@@ -0,0 +1,13 @@
+# boolem
+
+is_boiling = True
+string = 8
+total = string + is_boiling # upcasting  
+print(f"total {total}" )
+
+water_hot = True
+tea_added = False
+
+can_serve = water_hot and tea_added
+
+print(f"can serve chai?  {can_serve}")
---
+++ 02_datatypes/chapter_1.py
@@ -0,0 +1,26 @@
+#Interger
+black_tea_grams =14
+ginger_tea =  3
+
+total_gram = black_tea_grams + ginger_tea
+print(f"total gram {total_gram}")
+
+
+remaing_tea = black_tea_grams - ginger_tea
+print(f"total gram {remaing_tea}")
+
+milk_litres = 7
+serving = 4
+milk_per_serving = milk_litres / serving
+print(f"Milk per serving is {milk_per_serving}")
+
+total_tea_bag = 7
+pots = 4
+bags_per_pot = total_tea_bag // pots  # if u not career about decimal use [//]
+print(f"while tea par bag {bags_per_pot}")
+
+total_cadamom_ = 10
+pods_per = 3
+leftover_pod = total_cadamom_% pods_per  #for reminder use %
+print(f"leftover c {leftover_pod}")
+
