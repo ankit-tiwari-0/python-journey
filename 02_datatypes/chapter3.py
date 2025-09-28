b9f1f41a91714b4c2df902fd4a62a705e02a9ff7
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2025-09-28 17:37:49 +0530

    git add

--- 02_datatypes/chapter3.py
+++ 02_datatypes/chapter3.py
@@ -2,3 +2,8 @@ chai_type = "ginger chai"
 customer_name = "Noorin"
 
 print(f"order for {customer_name} : {chai_type} please")
+
+
+chai_description = "Aromatic and Bold"
+print(f"First word word: {chai_description[:7]}")
+print(f"last word word: {chai_description[12:]}")
