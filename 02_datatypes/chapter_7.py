8db8bfcebbd256cd529f25d9497569178114fbf4
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2025-09-30 10:30:58 +0530

    add

--- 02_datatypes/chapter_7.py
+++ 02_datatypes/chapter_7.py
@@ -2,3 +2,15 @@
 
 chai_order = dict(type="Masala chai", size="Large", sugar=3)
 print(f"chai order: {chai_order}")
+
+chai_recipe = {}
+chai_recipe["base"] = "black tea"
+chai_recipe["liquid"] = "milk"
+
+print(f"recipe base: {chai_recipe['base']}")
+print(f"recipe:{ chai_recipe}")
+del chai_recipe["liquid"]
+print(f"recipe : {chai_recipe}")
+
+
+print(f"Is sugar in the order {'type' in chai_order}")
