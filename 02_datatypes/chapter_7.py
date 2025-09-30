c84d35d70654482af06feef372b9afade2321a62
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2025-09-30 11:07:28 +0530

    git Add Dictionary

--- 02_datatypes/chapter_7.py
+++ 02_datatypes/chapter_7.py
@@ -14,3 +14,18 @@ print(f"recipe : {chai_recipe}")
 
 
 print(f"Is sugar in the order {'type' in chai_order}")
+
+chai_order = {"typer": "Ginger chai", "size": "Mediam", "sugar": 1}
+
+print(f"order detail (key): {chai_order.keys()}")
+print(f"order detail (key): {chai_order.values()}")
+
+last_item = chai_order.popitem()
+print(f"Remove last item: {last_item}")
+
+extra_spices = {"cardamom": "crushed","ginger": "sliced"}
+chai_recipe.update(extra_spices)
+print(f"extra_spice {chai_recipe}")
+
+customer_note = chai_order.get("NOte", "NO NOTE")
+print("customer_note is", customer_note)
