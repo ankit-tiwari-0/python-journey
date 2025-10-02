4e6006975763e8334d8eaa8003a1240410acea4f
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2025-10-02 17:57:51 +0530

    add chai

---
+++ 03_conditionals/chai_price_3.py
@@ -0,0 +1,11 @@
+cup = input("choose you cup size (small/mediem/large)::").lower()
+   
+if cup == "small":
+    print("Price is $1 dollor")
+elif cup == "mediam":
+   print("price is $2 dollor")
+elif cup == "large":
+    print("price is $5 dollors")   
+
+else:
+    print("bhaaaaag")
