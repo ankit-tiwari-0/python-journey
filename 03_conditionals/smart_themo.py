5e18d8df4a6a9f6eb137ad0173dbfd8da7bc1ba1
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2025-10-02 18:15:32 +0530

    git add this

---
+++ 03_conditionals/smart_themo.py
@@ -0,0 +1,11 @@
+device_statust = "active"
+temperature = 30
+
+if device_statust == "active":
+    if temperature > 35:
+     print("High tem")
+    else:
+     print("tem is norma;")
+
+else:
+    print("Device is offial")
