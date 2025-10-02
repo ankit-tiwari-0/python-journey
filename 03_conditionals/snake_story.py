a648bbd513590718ea1b851391017c8c791048d1
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2025-10-02 17:37:07 +0530

    git add snack

---
+++ 03_conditionals/snake_story.py
@@ -0,0 +1,8 @@
+snack = input("Enter your preferred snack: ").lower()
+
+
+if snack == "cookies" or snack == "smmosa" or snack == "chai":
+    print(f"Great choice! we'll serve you {snack}")
+
+else:
+    print("sorry")    
