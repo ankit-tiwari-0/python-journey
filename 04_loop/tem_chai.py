f13d48e89eb7575cd8f8215761e1d524bb58e73b
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2025-10-04 18:01:51 +0530

    git add while loop

---
+++ 04_loop/tem_chai.py
@@ -0,0 +1,11 @@
+#while loop
+
+temperature = 40
+
+while temperature <= 100:
+    print(f"current temperature: {temperature}")
+    #temperature = temperature + 10
+    temperature +=10
+
+
+print(f"Tea is ready to boil")
