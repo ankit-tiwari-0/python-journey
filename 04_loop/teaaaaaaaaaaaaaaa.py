16177c93413dcde63abf05c2dea335a1943b4962
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2025-10-04 16:45:51 +0530

    add naa

---
+++ 04_loop/teaaaaaaaaaaaaaaa.py
@@ -0,0 +1,9 @@
+order =["hitesh", "Aman", "Becky", "Carlos"]
+
+for nmae in order:
+    print(f"order ready for {nmae}")
+
+name =["ggg", "jhhh", "hhhjj","hhhss","jjjj"]
+
+for nnnn in order:
+    print("chal reeeeeeeeeee", nnnn)
