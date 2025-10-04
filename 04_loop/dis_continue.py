03b564f6cc1e595967acd6fbdf3ca724678eca20
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2025-10-04 18:24:47 +0530

    break, continue

---
+++ 04_loop/dis_continue.py
@@ -0,0 +1,16 @@
+# break , continue
+
+flavours = ["ginger", "out of stock", "Lemon", "Discontinued", "Tulsi"]
+
+
+
+
+
+for  flavour in flavours:
+    if flavour == "out of stock":
+        continue
+    if flavour == "Discontinued":
+        break
+    print(f"{flavour} item found")
+
+print(f"Out side of loop")
