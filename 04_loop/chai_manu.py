5b23a5eaa58c0087128a0f51427fa67097dacb61
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2025-10-04 17:13:50 +0530

    add naaa

---
+++ 04_loop/chai_manu.py
@@ -0,0 +1,13 @@
+menu = ["Green", "Lemon", "Spice", "Mint", "Kuladh"]
+
+for idx, item in enumerate(menu, start = 1):
+    print(f"{idx} : {item} chsi")
+
+
+
+
+students = ["hh", "jj","jjj","jjjj", "jjjj", "hhh",]
+
+for ix, tem in enumerate(students, start = 0):
+    print(f"{ix} : {tem} students")
+
