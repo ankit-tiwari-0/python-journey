10c7a452a2695fdb2dd4cb5486f1a0f512d367ae
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2025-10-06 20:39:19 +0530

    function

---
+++ 05_function/complex.py
@@ -0,0 +1,18 @@
+def fetch_sale():
+    print("Fetching the sales data")
+
+
+def filter_valid_sales():
+    print("Filtering valid sales data")
+
+def summarize_data():
+    print("summarizing sales data")
+
+def generate_report():
+    fetch_sale()  
+    filter_valid_sales()
+    summarize_data()
+    print("report is ready")
+
+
+generate_report()
---
+++ 05_function/duplicateion.py
@@ -0,0 +1,8 @@
+def print_order(name, chai_type):
+    print(f"{name} ordered {chai_type} chai")
+
+print_order("Aman", "masala")
+print_order("Aman", "masala")
+print_order("Aman", "masala")
+print_order("Aman", "masala")
+print_order("Aman", "masala")
