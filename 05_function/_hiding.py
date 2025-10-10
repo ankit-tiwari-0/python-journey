66289459798040a1d426e3da738bc6808c89178e
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2025-10-10 21:32:32 +0530

    registers user

---
+++ 05_function/_hiding.py
@@ -0,0 +1,20 @@
+def get_inpute():
+    print("Getting user input")
+
+def validation():
+    print("Validation the user info")
+
+def save_to_db():
+    print("Saving to database")
+
+def resigster_user():
+    get_inpute
+    validation 
+    save_to_db
+    print("User registration complete")
+
+    
+resigster_user()         
+
+
+#   defining the method 
