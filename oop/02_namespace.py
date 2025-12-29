3f76ef6c896e9a5f56474f6f976a754436beba55
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2025-12-29 22:36:02 +0530

    namespace

---
+++ oop/02_namespace.py
@@ -0,0 +1,14 @@
+class chai:
+    orgirn = "india"  
+print(chai.orgirn)
+
+chai.is_hot = True
+print(chai.is_hot)
+# 2line when variable goes under clas called object    
+#  it just a wrapper u have function , variable, generater
+
+# creating objects from class chai  
+
+masala = chai()
+print(masala.orgirn)
+print(masala.is_hot)
